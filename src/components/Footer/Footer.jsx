3ccec681fca3309d0ac935c8d8c2d11f3237eae8
Ary Pratama <ary.pratama@indivaragroup.com>
2023-03-25 12:27:21 +0700

    adding header and footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,223 @@
+import React from 'react'
+import './Footer.css'
+import logoFooter from '../../assets/images/logo-footer.png'
+import iconWa from '../../assets/icons/wa.png'
+import iconFb from '../../assets/icons/fb.png'
+import iconIg from '../../assets/icons/ig.png'
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="text-center text-lg-start bg-blue">
+                {/* <!-- Section: Social media --> */}
+                <section className="d-flex justify-content-center justify-content-lg-between">
+                </section>
+                {/* <!-- Section: Social media --> */}
+
+                {/* <!-- Section: Links  --> */}
+                <section className="border-bottom">
+                    <div className="container text-center text-md-start mt-5">
+                        {/* <!-- Grid row --> */}
+                        <div className="row mt-3">
+                            {/* <!-- Grid column --> */}
+                            <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                                {/* <!-- Content --> */}
+                                <div className='mb-5'>
+                                    <img className="mx-auto" src={logoFooter}></img>
+                                </div>
+
+                                <h6 className="text-uppercase fw-bold mb-4">
+                                    Mayapada Hospital Jakarta Selatan
+                                </h6>
+                                <p>
+                                    Jl. Lebak Bulus 1, Kav. 29
+                                    <br></br>
+                                    Jakarta Selatan
+                                </p>
+                                {/* <!-- Grid column --> */}
+                            </div>
+
+                            {/* <!-- Grid column --> */}
+
+                            {/* <!-- Grid column --> */}
+                            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                                {/* <!-- Links --> */}
+                                <h6 className="text-uppercase fw-bold mb-4">
+                                    Tentang Kami
+                                </h6>
+                                <p>
+                                    <a href="#!" className="text-reset">Sejarah</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Visi Misi</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Nilai Utama</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Landasan Hukum</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Akreditasi</a>
+                                </p>
+                            </div>
+                            {/* <!-- Grid column --> */}
+
+                            {/* <!-- Grid column --> */}
+                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                                {/* <!-- Links --> */}
+                                <h6 className="text-uppercase fw-bold mb-4">
+                                    AKADEMIK
+                                </h6>
+                                <p>
+                                    <a href="#!" className="text-reset">Program Studi</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">D3 Keperawatan</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">S1 Admin RS</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Profesi NERS</a>
+                                </p>
+                            </div>
+                            {/* <!-- Grid column --> */}
+
+                            {/* <!-- Grid column --> */}
+                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                                {/* <!-- Links --> */}
+                                <h6 className="text-uppercase fw-bold mb-4">
+                                    UPMI
+                                </h6>
+                                <p>
+                                    <a href="#!" className="text-reset">Kebijakan SPMI</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Manual SPMI</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Standar SPMI</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">SOP</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Formulir SPMI</a>
+                                </p>
+                            </div>
+                            {/* <!-- Grid column --> */}
+
+                            {/* <!-- Grid column --> */}
+                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                                {/* <!-- Links --> */}
+                                <h6 className="text-uppercase fw-bold mb-4">
+                                    FASILITAS
+                                </h6>
+                                <p>
+                                    <a href="#!" className="text-reset">Sarana Prasarana</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Kegiatan</a>
+                                </p>
+                            </div>
+                            {/* <!-- Grid column --> */}
+
+                            {/* <!-- Links --> */}
+                            <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                                <h6 className="text-uppercase fw-bold">
+
+                                    Hubungi Kami
+                                </h6>
+                                <p>
+                                    <a href="#!" className="text-reset">E-Mail: info@antariksa.co.id</a>
+                                </p>
+                                <div>
+                                    <a href="" class="me-4 text-reset">
+                                        <img src={iconWa}></img>
+                                    </a>
+                                    <a href="" class="me-4 text-reset">
+                                        <img src={iconFb}></img>
+                                    </a>
+                                    <a href="" class="me-4 text-reset">
+                                        <img src={iconIg}></img>
+                                    </a>
+                                </div>
+                            </div>
+                            {/* <!-- Grid column --> */}
+
+                            {/* <!-- Grid column --> */}
+                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                                {/* <!-- Links --> */}
+                                <h6 className="text-uppercase fw-bold mb-4">
+                                    ALUMNI
+                                </h6>
+                                <p>
+                                    <a href="#!" className="text-reset">Ikatan Alumni</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Testimoni</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">Tracer Studi</a>
+                                </p>
+                            </div>
+                            {/* <!-- Grid column --> */}
+
+                            {/* <!-- Grid column --> */}
+                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                                {/* <!-- Links --> */}
+                                <h6 className="text-uppercase fw-bold mb-4">
+                                    PENELITIAN
+                                </h6>
+                                <p>
+                                    <a href="#!" className="text-reset">Pengabdian</a>
+                                </p>
+                                <p>
+                                    <a href="#!" className="text-reset">E-Journal</a>
+                                </p>
+                            </div>
+                            {/* <!-- Grid column --> */}
+
+                            {/* <!-- Grid column --> */}
+                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                                {/* <!-- Links --> */}
+                                <h6 className="text-uppercase fw-bold mb-4">
+                                    PUSAT KARIR
+                                </h6>
+                                <p>
+                                    <a href="#!" className="text-reset">Info Karir</a>
+                                </p>
+                            </div>
+                            {/* <!-- Grid column --> */}
+
+                            {/* <!-- Grid column --> */}
+                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                                {/* <!-- Links --> */}
+                                <h6 className="text-uppercase fw-bold mb-4">
+                                    Informasi Pendaftaran Baru
+                                </h6>
+                            </div>
+                            {/* <!-- Grid column --> */}
+
+                        </div>
+                        {/* <!-- Grid row --> */}
+                    </div>
+                </section>
+                {/* <!-- Section: Links  --> */}
+
+                {/* <!-- Copyright --> */}
+                <div className="mb-4 text-white mt-4 text-center">
+                    Copyright ©  2023 Mayapada Nursing Academy. All rights reserved.
+                </div>
+                <div className='pb-3'>
+
+                </div>
+                {/* <!-- Copyright --> */}
+            </footer>
+            {/* <!-- Footer --> */}
+        </div >
+    )
+}
+
+export default Footer
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import './Header.css'
+import logo from '../../assets/images/logo.png'
+
+const Header = () => {
+    return (
+        <div className="text-center mt-5 mb-5">
+            <img className="mx-auto" src={logo}></img>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item px-2">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item dropdown px-2">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Akademik
+                                </a>
+                                <ul className="dropdown-menu dropdown-menu-dark rounded-0">
+                                    <li><a className="dropdown-item" href="#">Program D3 Keperawatan</a></li>
+                                    <li><a className="dropdown-item" href="#">Program S1 Administrasi</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown px-2">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    UPMI
+                                </a>
+                                <ul className="dropdown-menu dropdown-menu-dark rounded-0">
+                                    <li><a className="dropdown-item" href="#">Kebijakan SPMI</a></li>
+                                    <li><a className="dropdown-item" href="#">Manual SPMI</a></li>
+                                    <li><a className="dropdown-item" href="#">Standar SPMI</a></li>
+                                    <li><a className="dropdown-item" href="#">SOP</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown px-2">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Fasilitas
+                                </a>
+                                <ul className="dropdown-menu dropdown-menu-dark rounded-0">
+                                    <li><a className="dropdown-item" href="#">Sarana dan Prasarana</a></li>
+                                    <li><a className="dropdown-item" href="#">Gallery Kegiatan</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown px-2">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    UPPM
+                                </a>
+                                <ul className="dropdown-menu dropdown-menu-dark rounded-0">
+                                    <li><a className="dropdown-item" href="#">Penelitian</a></li>
+                                    <li><a className="dropdown-item" href="#">Pengabdian</a></li>
+                                    <li><a className="dropdown-item" href="#">E-Jurnal</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item px-2">
+                                <a className="nav-link" href="#">Pusat Karir</a>
+                            </li>
+                            <li className="nav-item dropdown px-2">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Alumni
+                                </a>
+                                <ul className="dropdown-menu dropdown-menu-dark rounded-0">
+                                    <li><a className="dropdown-item" href="#">Ikatan Alumni</a></li>
+                                    <li><a className="dropdown-item" href="#">Tracer Study</a></li>
+                                    <li><a className="dropdown-item" href="#">Alumni Story</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item px-2">
+                                <a className="nav-link" href="#">PMB</a>
+                            </li>
+                            <li className="nav-item px-2">
+                                <a className="nav-link" href="#">Hubungi Kami</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
