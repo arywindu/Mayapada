e76e158f3224467314ebcb826ff9ce99bbb1013f
Ary Pratama <ary.pratama@indivaragroup.com>
2023-05-16 15:19:51 +0800

    Adding section Home

--- src/pages/Admin/Dashboard/HomeAdmin.jsx
+++ src/pages/Admin/Dashboard/HomeAdmin.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./HomeAdmin.css";
 import LayoutAdmin from "../../../Layout/LayoutAdmin";
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 const HomeAdmin = () => {
     return (
@@ -68,7 +70,78 @@ const HomeAdmin = () => {
                             </div>
                         </div>
                         <hr class="bg-danger border-2 border-top border-dark mt-2 mb-4"></hr>
-
+                        <div class="mb-3 row">
+                            <label for="" class="col-sm-2 col-form-label">Section Visi Misi</label>
+                            <div class="col-sm-10">
+                                <div class="input-group mb-3">
+                                    <div className="App">
+                                        <CKEditor
+                                            editor={ClassicEditor}
+                                            data=""
+                                            onReady={editor => {
+                                                // You can store the "editor" and use when it is needed.
+                                            }}
+                                            onChange={(event, editor) => {
+                                                const data = editor.getData();
+                                            }}
+                                            onBlur={(event, editor) => {
+                                            }}
+                                            onFocus={(event, editor) => {
+                                            }}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr class="bg-danger border-2 border-top border-dark mt-2 mb-4"></hr>
+                        <div class="mb-3 row">
+                            <label for="" class="col-sm-2 col-form-label">Section Landasan Hukum</label>
+                            <div class="col-sm-10">
+                                <div class="input-group mb-3">
+                                    <div className="App">
+                                        <CKEditor
+                                            editor={ClassicEditor}
+                                            data=""
+                                            onReady={editor => {
+                                                // You can store the "editor" and use when it is needed.
+                                            }}
+                                            onChange={(event, editor) => {
+                                                const data = editor.getData();
+                                            }}
+                                            onBlur={(event, editor) => {
+                                            }}
+                                            onFocus={(event, editor) => {
+                                            }}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr class="bg-danger border-2 border-top border-dark mt-2 mb-4"></hr>
+                        <div class="mb-3 row">
+                            <label for="" class="col-sm-2 col-form-label">Section Struktur Organisasi</label>
+                            <div class="col-sm-10">
+                                <div class="input-group mb-3">
+                                    <div className="App">
+                                        <CKEditor
+                                            editor={ClassicEditor}
+                                            data=""
+                                            onReady={editor => {
+                                                // You can store the "editor" and use when it is needed.
+                                            }}
+                                            onChange={(event, editor) => {
+                                                const data = editor.getData();
+                                            }}
+                                            onBlur={(event, editor) => {
+                                            }}
+                                            onFocus={(event, editor) => {
+                                            }}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr class="bg-danger border-2 border-top border-dark mt-2 mb-4"></hr>
                     </div>
                 </div>
             </div>
