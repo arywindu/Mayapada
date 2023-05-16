4968e61e24e267ba08d933bb8b02f2b7268d332b
Ary Pratama <ary.pratama@indivaragroup.com>
2023-05-16 14:16:04 +0800

    Adding active on status home page

--- src/pages/Admin/Dashboard/HomeAdmin.jsx
+++ src/pages/Admin/Dashboard/HomeAdmin.jsx
@@ -7,6 +7,23 @@ const HomeAdmin = () => {
         <LayoutAdmin>
             <div class="Home-Admin">
                 <div class="row mt-5 mb-5" style={{ margin: "3% 10% 10% 10%" }}>
+                    <hr class="bg-danger border-2 border-top border-dark mt-1 mb-4"></hr>
+                    <div class="mb-3 row">
+                        <label for="" class="col-sm-2 col-form-label">Active Page</label>
+                        <div class="col-sm-10">
+                            <div class="input-group mb-3">
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="active" />
+                                    <label class="form-check-label" for="inlineRadio1">Active</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="disabled" />
+                                    <label class="form-check-label" for="inlineRadio2">Disabled</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr class="bg-danger border-2 border-top border-dark mt-2 mb-4"></hr>
                     <div class="mb-3 row">
                         <label for="staticEmail" class="col-sm-2 col-form-label">Logo Image</label>
                         <div class="col-sm-10">
