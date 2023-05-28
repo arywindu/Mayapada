d999deccca98d639e1b643270499149ac15bba8b
Trigha <adhindyatrigha@gmail.com>
2023-05-29 04:32:57 +0700

    wiring front ui akademik

--- src/pages/Akademik/Keperawatan/Keperawatan.jsx
+++ src/pages/Akademik/Keperawatan/Keperawatan.jsx
@@ -1,52 +1,132 @@
-import "./Keperawatan.css";
-import Layout from "../../../Layout/Layout";
-import React from "react";
-import AdmImage from "../../../assets/images/LandingKeperawatan.png";
-import KepImage01 from "../../../assets/images/Keperawatan01.png";
-import Typhography from "../../../components/Typhography/Typhography";
-import MatkulImg1 from "../../../assets/images/matkul1.png"
-import MatkulImg2 from "../../../assets/images/matkul2.png"
-import MatkulImg3 from "../../../assets/images/matkul3.png"
-import MatkulImg4 from "../../../assets/images/matkul4.png"
-import MatkulImg5 from "../../../assets/images/matkul5.png"
-import MatkulImg6 from "../../../assets/images/matkul6.png"
+import './Keperawatan.css';
+import Layout from '../../../Layout/Layout';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import AdmImage from '../../../assets/images/LandingKeperawatan.png';
+import KepImage01 from '../../../assets/images/Keperawatan01.png';
+import Typhography from '../../../components/Typhography/Typhography';
+import MatkulImg1 from '../../../assets/images/matkul1.png';
+import MatkulImg2 from '../../../assets/images/matkul2.png';
+import MatkulImg3 from '../../../assets/images/matkul3.png';
+import MatkulImg4 from '../../../assets/images/matkul4.png';
+import MatkulImg5 from '../../../assets/images/matkul5.png';
+import MatkulImg6 from '../../../assets/images/matkul6.png';
 
 const dataArray = [
   {
     img: MatkulImg1,
-    name: "Keperawatan Medikal Bedah",
+    name: 'Keperawatan Medikal Bedah',
   },
   {
     img: MatkulImg2,
-    name: "Bahasa Inggris & Bahasa Jepang",
+    name: 'Bahasa Inggris & Bahasa Jepang',
   },
   {
     img: MatkulImg3,
-    name: "Kewirausahaan",
+    name: 'Kewirausahaan',
   },
   {
     img: MatkulImg4,
-    name: "Keperawatan Gawat Darurat",
+    name: 'Keperawatan Gawat Darurat',
   },
   {
     img: MatkulImg5,
-    name: "Manajemen Keselamatan Pasien",
+    name: 'Manajemen Keselamatan Pasien',
   },
   {
     img: MatkulImg6,
-    name: "Manajemen Keperawatan",
+    name: 'Manajemen Keperawatan',
   },
 ];
 
 const Keperawatan = () => {
+  const token = localStorage.getItem('token');
+  const [data, setData] = useState(null);
+  const [jadwal, setJadwal] = useState(null);
+  const [kuliah, setKuliah] = useState(null);
+  const [dataFotoKuliah, setDataFotoKuliah] = useState(null);
+  const [selectedJadwal, setSelectedJadwal] = useState({});
+  const [banner, setBanner] = useState(null);
+  const [textBanner, setTextBanner] = useState(null);
+  const [imgVisiMisi, setImgVisiMisi] = useState(null);
+  const [textMisi, setTextMisi] = useState(null);
+  const [textVisi, setTextVisi] = useState(null);
+  const [textKurikulum, setTextKurikulum] = useState(null);
+  const [titleMataKuliah, setTitleMataKuliah] = useState(null);
+  const [fotoMataKuliah, setFotoMataKuliah] = useState(null);
+  const [fileKuliah, setFileKuliah] = useState(null);
+  const [titleText, setTitletext] = useState([]);
+
+  useEffect(() => {
+    getData();
+    getJadwal();
+    getKuliah();
+    getFotoKuliah();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        'https://stikesmayapada.ac.id/api/akademik-keperawatan/1'
+      );
+      const responseData = response.data;
+      console.log(responseData);
+      setBanner(responseData.banner_image);
+      setTextBanner(responseData.text_banner);
+      setImgVisiMisi(responseData.image_visi_misi);
+      setTextVisi(responseData.text_visi);
+      setTextMisi(responseData.text_misi);
+      setTextKurikulum(responseData.text_kurikulum);
+      setTitleMataKuliah(responseData.title_mata_kuliah);
+      setData(responseData);
+    } catch (error) {}
+  };
+  const getJadwal = async () => {
+    try {
+      const response = await axios.get(
+        'https://stikesmayapada.ac.id/api/akademik-keperawatan/files/all?selectBy=KURIKULUM '
+      );
+      const responseData = response.data;
+      const images = responseData.map((item) => item.file_path);
+      setSelectedJadwal(images);
+      setJadwal(responseData);
+    } catch (error) {}
+  };
+  const getKuliah = async () => {
+    try {
+      const response = await axios.get(
+        'https://stikesmayapada.ac.id/api/akademik-keperawatan/files/all?selectBy=JADWAL'
+      );
+      const responseData = response.data;
+      const images = responseData.map((item) => item.file_path);
+      setFileKuliah(images);
+      setKuliah(responseData);
+    } catch (error) {}
+  };
+
+  const getFotoKuliah = async () => {
+    try {
+      const response = await axios.get(
+        'https://stikesmayapada.ac.id/api/akademik-keperawatan/images/all'
+      );
+      const responseData = response.data;
+      const images = responseData.map((item) => item.image);
+      const descriptions = responseData.map((item) => item.description);
+
+      setFotoMataKuliah(images);
+      setTitletext(descriptions);
+
+      setDataFotoKuliah(responseData);
+    } catch (error) {}
+  };
+  console.log(data, 'data');
   return (
     <Layout>
       <div>
-        <img src={AdmImage} width={'100%'}>
-        </img>
+        <img src={AdmImage} width={'100%'}></img>
         <div class="centered-keperawatan">
           <h1 className="text-white">Program Studi DIII Keperawatan</h1>
-          <p className="text-white mt-4">Diploma III Keperawatan adalah program studi yang menghasilkan perawat professional pemula yang berintegritas dalam memberikan asuhan keperawatan dengan menerapkan ilmu-ilmu untuk kepentingan peningkatan kesehatan masyarakat.. </p>
+          <p className="text-white mt-4">{data && data.text_banner}</p>
         </div>
       </div>
       <div class="container bg-color-adm">
@@ -57,22 +137,15 @@ const Keperawatan = () => {
 
           <div class="col-md-6">
             <h2 className="text-blue">Visi</h2>
-            <p style={{ textAlign: 'left' }}>Menghasilkan lulusan Sarjana Administrasi Rumah Sakit yang unggul dan berorientasi pada peningkatan mutu pelayanan kesehatan berdasarkan nilai komitmen, integritas, professional, berbelas kasih dan dapat dipercaya.</p>
+            <div
+              style={{ textAlign: 'left' }}
+              dangerouslySetInnerHTML={{ __html: data && data.text_visi }}
+            ></div>
             <h2 className="text-blue mt-5">Misi</h2>
-            <ol style={{ textAlign: 'left' }}>
-              <li className="mb-4">
-                Menyelenggarakan program pendidikan Administrasi Rumah Sakit yang bermutu tinggi berorientasi pada mutu pelayanan kesehatan berbasis teknologi informasi;
-              </li>
-              <li className="mb-4">
-                Meningkatkan penelitian dan pengabdian masyarakat yang inovativ dibidang administrasi rumah sakit;
-              </li>
-              <li className="mb-4">
-                Mengembangkan networking dan kemitraan dengan lembaga dalam negeri dan luar negeri untuk meningkatkan kompetensi dan pendayagunaan lulusan;
-              </li>
-              <li className="mb-4">
-                Melaksanakan semangat pendidikan berdasarkan nilai komitmen, integritas, professional, berbelas kasih dan dapat dipercaya.
-              </li>
-            </ol>
+            <div
+              style={{ textAlign: 'left' }}
+              dangerouslySetInnerHTML={{ __html: data && data.text_misi }}
+            ></div>
           </div>
         </div>
       </div>
@@ -81,11 +154,10 @@ const Keperawatan = () => {
         <div className="centered-keperawatan-section">
           <div className="container mt-5 pt-5">
             <h1 className="text-blue mt-5 pt-5">Kurikulum DIII Keperawatan</h1>
-            <p className="text-dark mt-3 text-wrap" style={{ width: '20rem' }}>Kurikulum DIII Keperawatan dengan Jumlah 112 SKS ditempuh  dalam 6 Semester (3 tahun) dengan Keterampilan Unggul Keperawatan Medikal Bedah Kurikulum Dibagi menjadi 2 :</p>
-            <ul className="content-dot">
-              <li>Mata Kuliah Inti 90 SKS</li>
-              <li>Mata Kuliah Institusional 22 SKS</li>
-            </ul>
+            <div
+              style={{ textAlign: 'left' }}
+              dangerouslySetInnerHTML={{ __html: data && data.text_kurikulum }}
+            ></div>
           </div>
         </div>
       </div>
@@ -95,25 +167,25 @@ const Keperawatan = () => {
           <Typhography
             type="title"
             text="Mata Kuliah yang Ditawarkan"
-            style={{ alignSelf: "center" }}
+            style={{ alignSelf: 'center' }}
           />
           <div className="container">
             <div className="keperawatan-story-card">
-              {dataArray.map((item) => (
-                <div className="card-wrapper">
-                  <img className="card-img" src={item.img} />
-                  <div className="card-content">
-                    <Typhography
-                      type="normal"
-                      text={item.name}
-                      size="16px"
-                      fontWeight="600"
-                    />
+              {dataFotoKuliah &&
+                dataFotoKuliah.map((item) => (
+                  <div className="card-wrapper">
+                    <img className="card-img" src={item.image} />
+                    <div className="card-content">
+                      <Typhography
+                        type="normal"
+                        text={item.description}
+                        size="16px"
+                        fontWeight="600"
+                      />
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
             </div>
-
           </div>
         </div>
 
@@ -121,72 +193,26 @@ const Keperawatan = () => {
           <Typhography
             type="title"
             text="Kurikulum DIII Keperawatan"
-            style={{ alignSelf: "center" }}
+            style={{ alignSelf: 'center' }}
           />
           <div class="row justify-content-center">
-
-            <div class="col-lg-3 col-md-6 pt-4 mt-2">
-              <button type="button" class="btn btn-blue btn-lg">
-                <i class="icon-download">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i>
-                Semester I
-              </button>
-            </div>
-
-            <div class="col-lg-3 col-md-6 pt-4 mt-2">
-              <button type="button" class="btn btn-blue btn-lg">
-                <i class="icon-download">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i>
-                Semester II
-              </button>
-
-            </div>
-
-            <div class="col-lg-3 col-md-6 pt-4 mt-2">
-              <button type="button" class="btn btn-blue btn-lg">
-                <i class="icon-download">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i>
-                Semester III
-              </button>
-            </div>
-
-            <div class="col-lg-3 col-md-6 pt-4 mt-2">
-              <button type="button" class="btn btn-blue btn-lg">
-                <i class="icon-download">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i>
-                Semester IV
-              </button>
-            </div>
-
-          </div>
-
-          <div class="row justify-content-center">
-
-            <div class="col-lg-3 col-md-6 pt-4 mt-2">
-              <button type="button" class="btn btn-blue btn-lg">
-                <i class="icon-download">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i>
-                Semester V
-              </button>
-            </div>
-
-            <div class="col-lg-3 col-md-6 pt-4 mt-2">
-              <button type="button" class="btn btn-blue btn-lg">
-                <i class="icon-download">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i>
-                Semester VI
-              </button>
-
-            </div>
-
-            <div class="col-lg-3 col-md-6 pt-4 mt-2">
-              <button type="button" class="btn btn-blue btn-lg">
-                <i class="icon-download">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i>
-                Semester VIII
-              </button>
-            </div>
-
-            <div class="col-lg-3 col-md-6 pt-4 mt-2">
-              <button type="button" class="btn btn-blue btn-lg">
-                <i class="icon-download">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i>
-                Semester IX
-              </button>
-            </div>
-
+            {jadwal &&
+              jadwal.map((item, index) => (
+                <React.Fragment key={index}>
+                  <div class="col-lg-3 col-md-6 pt-4 mt-2">
+                    <button
+                      type="button"
+                      class="btn btn-blue btn-lg"
+                      onClick={() => window.open(item.file_path, '_blank')}
+                    >
+                      <i class="icon-download">
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                      </i>
+                      Semester {index + 1}
+                    </button>
+                  </div>
+                </React.Fragment>
+              ))}
           </div>
         </div>
 
@@ -194,78 +220,30 @@ const Keperawatan = () => {
           <Typhography
             type="title"
             text="Jadwal Perkuliahan DIII Keperawatan"
-            style={{ alignSelf: "center" }}
+            style={{ alignSelf: 'center' }}
           />
           <div class="row justify-content-center">
-
-            <div class="col-lg-3 col-md-6 pt-4 mt-2">
-              <button type="button" class="btn btn-blue btn-lg">
-                <i class="icon-download">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i>
-                Semester I
-              </button>
-            </div>
-
-            <div class="col-lg-3 col-md-6 pt-4 mt-2">
-              <button type="button" class="btn btn-blue btn-lg">
-                <i class="icon-download">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i>
-                Semester II
-              </button>
-
-            </div>
-
-            <div class="col-lg-3 col-md-6 pt-4 mt-2">
-              <button type="button" class="btn btn-blue btn-lg">
-                <i class="icon-download">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i>
-                Semester III
-              </button>
-            </div>
-
-            <div class="col-lg-3 col-md-6 pt-4 mt-2">
-              <button type="button" class="btn btn-blue btn-lg">
-                <i class="icon-download">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i>
-                Semester IV
-              </button>
-            </div>
-
-          </div>
-
-          <div class="row justify-content-center">
-
-            <div class="col-lg-3 col-md-6 pt-4 mt-2">
-              <button type="button" class="btn btn-blue btn-lg">
-                <i class="icon-download">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i>
-                Semester V
-              </button>
-            </div>
-
-            <div class="col-lg-3 col-md-6 pt-4 mt-2">
-              <button type="button" class="btn btn-blue btn-lg">
-                <i class="icon-download">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i>
-                Semester VI
-              </button>
-
-            </div>
-
-            <div class="col-lg-3 col-md-6 pt-4 mt-2">
-              <button type="button" class="btn btn-blue btn-lg">
-                <i class="icon-download">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i>
-                Semester VIII
-              </button>
-            </div>
-
-            <div class="col-lg-3 col-md-6 pt-4 mt-2">
-              <button type="button" class="btn btn-blue btn-lg">
-                <i class="icon-download">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</i>
-                Semester IX
-              </button>
-            </div>
-
+            {kuliah &&
+              kuliah.map((item, index) => (
+                <React.Fragment key={index}>
+                  <div class="col-lg-3 col-md-6 pt-4 mt-2">
+                    <button
+                      type="button"
+                      class="btn btn-blue btn-lg"
+                      onClick={() => window.open(item.file_path, '_blank')}
+                    >
+                      <i class="icon-download">
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                      </i>
+                      Semester {index + 1}
+                    </button>
+                  </div>
+                </React.Fragment>
+              ))}
           </div>
         </div>
       </div>
-
-
-    </Layout >
+    </Layout>
   );
 };
 
