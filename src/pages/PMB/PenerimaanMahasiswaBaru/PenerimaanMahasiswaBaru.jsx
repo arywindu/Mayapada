d02613873a0656ebdfc0bc3d93ee083ed1e443da
Muhammad Dhiya Udhin <mdudhin@Muhammads-MacBook-Pro.local>
2023-06-20 22:33:37 +0700

    fix issue 17, 18, 19, 20

--- src/pages/PMB/PenerimaanMahasiswaBaru/PenerimaanMahasiswaBaru.jsx
+++ src/pages/PMB/PenerimaanMahasiswaBaru/PenerimaanMahasiswaBaru.jsx
@@ -1,13 +1,14 @@
-import './PenerimaanMahasiswaBaru.css';
+import "./PenerimaanMahasiswaBaru.css";
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+
+import Button from "../../../components/Button/Button";
+import Layout from "../../../Layout/Layout";
 import { Link } from "react-router-dom";
-
-import Button from '../../../components/Button/Button';
-import Layout from '../../../Layout/Layout';
-import Typhography from '../../../components/Typhography/Typhography';
-import axios from 'axios';
-import penerimaanMahasiswa from '../../../assets/images/penerimaan_mahasiswa.png';
+import Typhography from "../../../components/Typhography/Typhography";
+import axios from "axios";
+import penerimaanMahasiswa from "../../../assets/images/penerimaan_mahasiswa.png";
+import pmbCard from "../../../assets/images/pmb.jpeg";
 
 const PenerimaanMahasiswa = () => {
   const [banner, setBanner] = useState(null);
@@ -15,7 +16,7 @@ const PenerimaanMahasiswa = () => {
   const [title, setTitle] = useState(null);
   const [semesterKeperawatan, setsemesterKeperawatan] = useState(null);
   const [semesterRs, setSemesterRs] = useState(null);
-  const isMobile = window.matchMedia('(max-width: 767px)').matches;
+  const isMobile = window.matchMedia("(max-width: 767px)").matches;
 
   useEffect(() => {
     getData();
@@ -24,7 +25,7 @@ const PenerimaanMahasiswa = () => {
   const getData = async () => {
     try {
       const response = await axios.get(
-        'https://api.stikesmayapada.ac.id/api/PMB/1'
+        "https://api.stikesmayapada.ac.id/api/PMB/1"
       );
       // console.log(response.data, 'res');
       const dataRes = response.data.data;
@@ -72,12 +73,14 @@ const PenerimaanMahasiswa = () => {
               />
 
               <Link to="/Pjb">
-                <button className='mt-3 btn-daftar btn btn-blue btn-lg"'
+                <button
+                  className='mt-3 btn-daftar btn btn-blue btn-lg"'
                   type="button"
                   data-bs-target="#carouselExampleDark"
-                >Daftar Sekarang</button>
+                >
+                  Daftar Sekarang
+                </button>
               </Link>
-
             </div>
           </div>
         </div>
@@ -88,7 +91,7 @@ const PenerimaanMahasiswa = () => {
               className="penerimaan-mahasiswa-card"
               style={{
                 backgroundImage: `url(
-                    https://s3-alpha-sig.figma.com/img/886d/c8f0/6f7234ec9eec2720d909600050772baa?Expires=1681084800&Signature=KbOmaSUESrKlCgW67QVzOvbXcTCeFjbZh2I6ywf73wcMhGigvxJGdlwwHvL0geFIB9bAoPGm0zdEbJEKHY0gPOHRl-EJ3Fwcy2RHFei0HueOqdYA8U~rmuCH-i3FoEJ8gWwzZoZK0NLNlF25VLFBI1bJf5XgxNIJwqr3443ZfICivLisSZoTdYNwApoBaZ6UM4YKZTr9HQH9ymkx8QKIASxDq2XcXBeO1LonY8Lnw3ytZYXUxVJwnybBaSw1AtkvvaNdSkXhkXgrtkUwZHBC~99KVea9qKb-KQ0WEg3GYHu1bB85S26ENnjN84nhUU89nnEe4SvokBAXa~w7lllvwg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4
+                  ${pmbCard}
                 )`,
               }}
             >
@@ -104,8 +107,8 @@ const PenerimaanMahasiswa = () => {
                   text="info lengkap"
                   type="btn-border"
                   borderColor="white"
-                  style={{ marginTop: '12px' }}
-                  onClick={(event) => (window.location.href = '/JalurMandiri')}
+                  style={{ marginTop: "12px" }}
+                  onClick={(event) => (window.location.href = "/JalurMandiri")}
                 />
               </div>
             </div>
@@ -130,8 +133,8 @@ const PenerimaanMahasiswa = () => {
                   text="info lengkap"
                   type="btn-border"
                   borderColor="white"
-                  style={{ marginTop: '12px' }}
-                  onClick={(event) => (window.location.href = '/JalurBeasiswa')}
+                  style={{ marginTop: "12px" }}
+                  onClick={(event) => (window.location.href = "/JalurBeasiswa")}
                 />
               </div>
             </div>
@@ -173,41 +176,44 @@ const PenerimaanMahasiswa = () => {
                     </tr>
                   </tbody>
                 </table>
-              </>) : <>
-              <table class="table table-bordered">
-                <tbody>
-                  <tr>
-                    <td>Semester I</td>
-                    <td>{semesterKeperawatan && semesterKeperawatan.s1}</td>
-                  </tr>
-                  <tr>
-                    <td>Semester II</td>
-                    <td>{semesterKeperawatan && semesterKeperawatan.s2}</td>
-                  </tr>
-                  <tr>
-                    <td>Semester III</td>
-                    <td>{semesterKeperawatan && semesterKeperawatan.s3}</td>
-                  </tr>
-                </tbody>
-              </table>
+              </>
+            ) : (
+              <>
+                <table class="table table-bordered">
+                  <tbody>
+                    <tr>
+                      <td>Semester I</td>
+                      <td>{semesterKeperawatan && semesterKeperawatan.s1}</td>
+                    </tr>
+                    <tr>
+                      <td>Semester II</td>
+                      <td>{semesterKeperawatan && semesterKeperawatan.s2}</td>
+                    </tr>
+                    <tr>
+                      <td>Semester III</td>
+                      <td>{semesterKeperawatan && semesterKeperawatan.s3}</td>
+                    </tr>
+                  </tbody>
+                </table>
 
-              <table class="table table-bordered">
-                <tbody>
-                  <tr>
-                    <td>Semester IV</td>
-                    <td>{semesterKeperawatan && semesterKeperawatan.s4}</td>
-                  </tr>
-                  <tr>
-                    <td>Semester V</td>
-                    <td>{semesterKeperawatan && semesterKeperawatan.s5}</td>
-                  </tr>
-                  <tr>
-                    <td>Semester VI</td>
-                    <td>{semesterKeperawatan && semesterKeperawatan.s6}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </>}
+                <table class="table table-bordered">
+                  <tbody>
+                    <tr>
+                      <td>Semester IV</td>
+                      <td>{semesterKeperawatan && semesterKeperawatan.s4}</td>
+                    </tr>
+                    <tr>
+                      <td>Semester V</td>
+                      <td>{semesterKeperawatan && semesterKeperawatan.s5}</td>
+                    </tr>
+                    <tr>
+                      <td>Semester VI</td>
+                      <td>{semesterKeperawatan && semesterKeperawatan.s6}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </>
+            )}
           </div>
 
           <Typhography
@@ -247,8 +253,8 @@ const PenerimaanMahasiswa = () => {
                     </tr>
                   </tbody>
                 </table>
-              </>)
-              :
+              </>
+            ) : (
               <>
                 <table class="table table-bordered">
                   <tbody>
@@ -283,7 +289,8 @@ const PenerimaanMahasiswa = () => {
                     </tr>
                   </tbody>
                 </table>
-              </>}
+              </>
+            )}
           </div>
         </div>
       </div>
